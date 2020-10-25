7cbd0aafcdecb26f0fb0272b290bba96fe081579
Austin Albrecht <austin.aa@me.com>
2020-10-25 12:38:30 +0100

    Make Profile.vue prototype ready

--- src/views/Profile.vue
+++ src/views/Profile.vue
@@ -4,67 +4,61 @@
       <div class="column is-half">
         <div class="centered-image">
           <figure class="image is-128x128 is-centered">
-            <img class="is-rounded" src="https://payload.cargocollective.com/1/23/749547/12784313/IDK_800.png" alt="Profilbild">
+            <img class="is-rounded" src="http://www.colegiodepadua.com.br/img/user.png" alt="Profilbild">
           </figure>
         </div>
 
-        <button class="button">Avatar hochladen</button>
+        <button disabled class="button">Avatar hochladen</button>
 
         <div class="field has-text-left">
           <label class="label">Email</label>
           <div class="control">
-            <input class="input" v-model="profile.email" type="mail" placeholder="mail@provider.tld">
+            <input disabled class="input" v-model="displayedProfile.email" type="mail" placeholder="mail@provider.tld">
           </div>
         </div>
 
         <div class="field has-text-left">
           <label class="label">Passwort</label>
           <div class="control">
-            <input class="input" v-model="profile.password" type="password" placeholder="hunter2">
+            <input class="input" v-model="displayedProfile.password" type="password" placeholder="hunter2">
           </div>
         </div>
 
         <div class="field has-text-left">
           <label class="label">Passwort wiederholen</label>
           <div class="control">
-            <input class="input" v-model="profile.passwordRepeat" type="password" placeholder="hunter2">
+            <input class="input" v-model="displayedProfile.passwordRepeat" type="password" placeholder="hunter2">
           </div>
         </div>
 
         <div class="field has-text-left">
           <label class="label">Ich will helfen</label>
-          <switch-checkbox v-model="profile.helper"></switch-checkbox>
+          <switch-checkbox v-model="displayedProfile.helper"></switch-checkbox>
         </div>
 
-        <div class="field has-text-left" v-if="profile.helper">
+        <div class="field has-text-left" v-if="displayedProfile.helper">
           <label class="label">Kategorien</label>
           <div class="control">
             <div class="select is-multiple is-fullwidth">
-              <select multiple size="5" v-model="profile.categories">
-                <option value="Englisch">Englisch</option>
-                <option value="Deutsch">Deutsch</option>
-                <option value="Kat1">Kat1</option>
-                <option value="Kat2">Kat2</option>
-                <option value="Etc">Etc</option>
-                <option value="Other">Other</option>
-                <option value="Pog">Pog</option>
+              <select multiple size="5" v-model="displayedProfile.categories">
+                <option v-for="cat in availableCategories" :key="cat.name" :value="cat">{{cat.name}}</option>
               </select>
             </div>
           </div>
           <p class="help">Mehrere mit CTRL+Click auswählen</p>
         </div>
 
-        <div class="field has-text-left" v-if="profile.helper">
+        <div class="field has-text-left" v-if="displayedProfile.helper">
           <label class="label">Tags</label>
           <div class="control">
-            <input class="input" v-model="profile.tags" type="text" placeholder="z.B. sbb, billet, zvv">
+            <input disabled class="input" v-model="displayedProfile.tags" type="text" placeholder="z.B. sbb, billet, zvv">
           </div>
         </div>
 
-        <div class="field has-text-left" v-if="profile.helper">
+        <div class="field has-text-left" v-if="displayedProfile.helper">
           <label class="label">Über mich</label>
           <div class="control">
-            <textarea class="textarea" v-model="profile.bio" placeholder="I am a massive chad and will help with anything."></textarea>
+            <textarea class="textarea" v-model="displayedProfile.biographie" placeholder="I am a massive chad and will help with anything."></textarea>
           </div>
         </div>
 
@@ -72,11 +66,11 @@
 
         <div class="level">
           <div class="level-left">
-            <button class="button is-danger">Löschen</button>
+            <button disabled class="button is-danger">Löschen</button>
           </div>
           <div class="level-right">
             <div class="buttons">
-              <button class="button is-info" :disabled="!this.isValid">Profil speichern</button>
+              <button class="button is-info" @click="saveUser(displayedProfile)" :disabled="!this.isValid">Profil speichern</button>
               <button class="button" @click="$router.go(-1)">Abbrechen</button>
             </div>
           </div>
@@ -96,23 +90,61 @@ export default {
   },
   data: function () {
     return {
-      profile: {
-        email: "austin@neat.moe",
+      displayedProfile: {
+        email: "",
         password: "",
         passwordRepeat: "",
-        helper: true,
+        helper: false,
         categories: [],
-        tags: "", // TODO: prolly has to be array later
+        tags: "", // TODO: has to be array later
         bio: ""
-      }
+      },
+      availableCategories: [],
+      selectedCategories: []
     }
   },
   computed: {
     isValid: function () {
-      let emailOK = this.profile.email.length > 0;
-      let passOK = this.profile.password === this.profile.passwordRepeat;
-      return emailOK && passOK;
+      let emailOK = this.displayedProfile.email.length > 0;
+      let passOK = this.displayedProfile.password === this.displayedProfile.passwordRepeat;
+      return true || emailOK && passOK; // TODO: validation properly
     }
+  },
+  methods: {
+    fetchUser: async function () { // TODO: This is used in many places. Put it somewhere more global & cache in localstorage
+      let currentUser = "ahmed_miri@gmx.net";
+      let res = await fetch(`http://${this.$baseURL}/api/user/${currentUser}`);
+      let user = await res.json();
+
+      user.password = "";
+      this.displayedProfile.passwordRepeat = "";
+
+      this.displayedProfile.helper = user.status === "ACTIVE";
+
+      return user;
+    },
+    fetchCategories: async function () { // TODO: Put it more global
+      let res = await fetch(`http://${this.$baseURL}/api/category/all`);
+      return res.json();
+    },
+    saveUser: async function (user) {
+      user.type = user.helper ? "Helper" : "Helpseeker";
+      let currentUser = "ahmed_miri@gmx.net";
+      let res = await fetch(`http://${this.$baseURL}/api/user/update/${currentUser}`, {
+        method: "PUT",
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(this.displayedProfile)
+      });
+
+      console.log(res);
+
+    }
+  },
+  beforeMount: async function() {
+    this.availableCategories = await this.fetchCategories();
+    this.displayedProfile = await this.fetchUser();
   }
 }
 </script>
