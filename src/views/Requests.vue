644fcfde350a0c8157ae4c571db40fb78d235ed3
pedernin <pedernin@students.zhaw.ch>
2020-10-31 18:06:16 +0100

    Match manuel ausführen first draft #12

--- src/views/Requests.vue
+++ src/views/Requests.vue
@@ -49,12 +49,9 @@
             <td>{{ openJob.created }}</td>
             <td>
               <div class="buttons">
-                <button class="button" @click="deleteJob(openJobs, index)">
-                  Löschen
-                </button>
-                <button disabled class="button">
-                  Bearbeiten (WIP)
-                </button>
+                <button class="button" @click="deleteJob(openJobs, index)">Löschen</button>
+                <button class="button" @click="findMatch(openJobs, index)">Match</button>
+                <button disabled class="button">Bearbeiten (WIP)</button>
               </div>
             </td>
           </tr>
@@ -83,6 +80,39 @@
       </div>
     </div>
 
+    <div class="modal" v-bind:class="{ 'is-active': isModalOpen }">
+      <div class="modal-background"></div>
+      <div class="modal-card">
+        <header class="modal-card-head">
+          <p class="modal-card-title">Helper gefunden!</p>
+        </header>
+        <section class="modal-card-body">
+          <table class="table is-fullwidth is-hoverable">
+            <thead>
+            <tr>
+              <th>Helfer/in</th>
+              <th>Biografie</th>
+              <th>Standort</th>
+              <th>Auswählen</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr v-for="item in availableMatches" v-bind:key="item">
+              <td>{{ item.firstname + " " + item.lastname }}</td>
+              <td>{{ item.biographie }}</td>
+              <td>{{ item.plz }}</td>
+              <td>
+                <button class="button" @click="selectHelper(item)">Auswählen</button>
+              </td>
+            </tr>
+            </tbody>
+          </table>
+        </section>
+        <footer class="modal-card-foot">
+          <button class="button" @click="isModalOpen = false">Abbrechen</button>
+        </footer>
+      </div>
+    </div>
 
   </div>
 </template>
@@ -97,7 +127,9 @@ export default {
     return {
       openJobs: [],
       inProgressJobs: [],
-      closedJobs: []
+      availableMatches: [],
+      closedJobs: [],
+      isModalOpen: false,
     };
   },
   methods: {
@@ -121,6 +153,17 @@ export default {
       }
 
       console.log(jobs);
+    },
+    findMatch: async function(openJobs, i) {
+      try {
+          this.availableMatches = await api.findHelperForJobId(this.openJobs[i].id);
+          if(this.availableMatches.length > 0) {
+            this.isModalOpen = true;
+          }
+        } catch (e) {
+          // TODO: Tell user that no match was found
+          console.error(e);
+        }
     },
     deleteJob: async function(jobList, i) {
       try {
