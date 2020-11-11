9427495607568faf20618fc0f8fb59f3ea6f583a
pedernin <pedernin@students.zhaw.ch>
2020-11-11 18:27:21 +0100

    login form restyling

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="login">
     <h1>Login</h1>
-    <div class="login-form">
-      <input type="text" v-model="username" name="username" placeholder="Email" v-on:keyup.esc="cancel()" />
-      <input type="password" v-model="password" name="password" placeholder="Passwort" v-on:keyup.enter="login()" v-on:keyup.esc="cancel()" />
-      <div class="buttons">
-        <button type="button" v-on:click="login()">Login</button>
-        <button type="button" v-on:click="cancel()">Abbrechen</button>
+    <div class="columns">
+      <div class="column is-one-third is-offset-one-third has-text-left">
+        <input class="input" type="text" v-model="username" name="username" placeholder="Email" v-on:keyup.esc="cancel()" />
+        <input class="input" type="password" v-model="password" name="password" placeholder="Passwort" v-on:keyup.enter="login()" v-on:keyup.esc="cancel()" />
+        <div class="buttons">
+          <button class="button" v-on:click="login()">Login</button>
+          <button class="button" v-on:click="cancel()">Abbrechen</button>
+        </div>
       </div>
     </div>
   </div>
@@ -43,32 +45,6 @@ export default {
 }
 </script>
 
-<style>
-#login {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100%;
-}
+<style scoped>
 
-.login-form {
-  width: 20%;
-  display: flex;
-  flex-direction: column;
-}
-
-.login-form input {
-  padding: 5px 0px 5px 0px;
-  margin: 3px 0px 3px 0px;
-}
-
-.buttons {
-  display: flex;
-  flex-direction: row-reverse;
-}
-
-.buttons button {
-  margin-left: 3px;
-}
 </style>
