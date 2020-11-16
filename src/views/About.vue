99c8df0eeabd5e16f27f9f2923a766ced8b2f994
pedernin <pedernin@students.zhaw.ch>
2020-11-16 14:48:19 +0100

    star-rating proof of concept. can be discarded later on

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,62 +1,41 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    {{ tableData }}
-
-    <table class="table is-striped">
-      <thead>
-        <tr>
-          <th>Name</th>
-          <th>Email</th>
-          <th>Edit btn</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="item in tableData" :key="item.name">
-          <td v-if="!item.editing">{{ item.name }}</td>
-          <td v-if="!item.editing">{{ item.mail }}</td>
-
-          <td v-if="item.editing">
-            <input type="text" class="input" v-model="item.name" />
-          </td>
-          <td v-if="item.editing">
-            <input type="email" class="input" v-model="item.mail" />
-          </td>
-
-          <td v-if="!item.editing">
-            <button class="button" v-on:click="item.editing = !item.editing">
-              Edit
-            </button>
-          </td>
-          <td v-if="item.editing">
-            <button class="button" v-on:click="item.editing = !item.editing">
-              Save
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="test">
+      <vue-star-rating v-bind:increment="1"
+        v-bind:max-rating="5"
+        inactive-color="#000"
+        active-color="#621bbf"
+        v-bind:star-size="25"
+        @rating-selected ="setRating">
+      </vue-star-rating>
+    </div>
   </div>
 </template>
 
 <script>
+import VueStarRating from "vue-star-rating/src/star-rating";
+
 export default {
   name: "About",
+  components: {
+    VueStarRating
+  },
+  methods: {
+    setRating: function(rating){
+      this.rating = rating;
+    }
+  },
   data: function() {
     return {
-      tableData: []
-    };
-  },
-  beforeMount: function() {
-    console.log("Yes");
-
-    setTimeout(() => {
-      this.tableData = [
-        { name: "austin", mail: "austin@whatever", editing: false },
-        { name: "austin2", mail: "austin@whatever2", editing: false },
-        { name: "austin3", mail: "austin@whatever3", editing: false }
-      ];
-    }, 5000);
+      rating: 0
+    }
   }
 };
 </script>
+
+<style scoped>
+.test {
+  height: 100px;
+}
+</style>
