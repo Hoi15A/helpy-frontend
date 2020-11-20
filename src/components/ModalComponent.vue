e8dff5f4272c086ce2b421ba0f13167903206259
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-11-20 14:50:57 +0100

    Added ModalComponent component
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>
    Co-Authored-By: Austin Albrecht <2957+albreaus@users.noreply.github.zhaw.ch>

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="modal">
+    <div class="modal-background" @click="$emit('close-modal')"></div>
+    <div class="modal-card">
+      <header class="modal-card-head">
+        <p class="modal-card-title"><slot name="title"></slot></p>
+      </header>
+      <section class="modal-card-body">
+        <slot name="content"></slot>
+      </section>
+      <footer class="modal-card-foot">
+        <slot name="footer"></slot>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Modal"
+}
+</script>
