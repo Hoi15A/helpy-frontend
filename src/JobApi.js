7f6e75d5aa5f4cc4bfe0f227aad3cbd6c9295d85
pedernin <pedernin@students.zhaw.ch>
2020-11-20 10:40:50 +0100

    update functions and methods with super keyword

--- src/JobApi.js
+++ src/JobApi.js
@@ -14,13 +14,44 @@ class JobApi extends Api1 {
     }
 
     async updateJob (job) {
-        let res = await customFetch(`${apiBaseUrl}/job/update/${job.id}`, {
+        let res = await super.customFetch(`${Api1.getApiBaseUrl()}/job/update/${job.id}`, {
             method: "PUT",
             headers: {
                 'Content-Type': 'application/json;charset=utf-8'
             },
             body: JSON.stringify(job)
         });
-        if (!res.ok) throw new Error(parseError(await res.json));
+        if (!res.ok) throw new Error(this.parseError(await res.json));
+    }
+
+    async setHelperForJobId (jobId, helperEmail) {
+        let res = await super.customFetch(`${Api1.getApiBaseUrl()}/job/id/${jobId}/set-helper/${helperEmail}`, {
+            method: "PUT"
+        });
+        if (!res.ok) throw new Error(this.parseError(await res.json));
+    }
+
+    async findHelperForJobId (id) {
+        let res = await super.customFetch(`${Api1.getApiBaseUrl()}/job/id/${id}/find-helper`);
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error(`Leider konnte kein Helper für deinen Antrag gefunden werden.`);
+        }
+    }
+
+    async addJob (job) {
+        let res = await super.customFetch(`${Api1.getApiBaseUrl()}/job/add`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify(job)
+        });
+        if (res.ok) {
+            return res.json();
+        } else {
+            throw new Error(this.parseError(await res.json()));
+        }
     }
 }
