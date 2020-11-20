62e78c38ee9cf5effb87757013eacfa72445e2eb
Carl Lubojanski <lubojcar@students.zhaw.ch>
2020-11-20 14:50:04 +0100

    Added ClosedJobTable component
    Co-Authored-By: meletlea <3164+meletlea@users.noreply.github.zhaw.ch>
    Co-Authored-By: Austin Albrecht <2957+albreaus@users.noreply.github.zhaw.ch>

---
+++ src/components/ClosedJobTable.vue
@@ -0,0 +1,33 @@
+<template>
+    <table class="table is-fullwidth is-hoverable">
+        <thead class="has-text-left">
+            <tr>
+                <th>Titel</th>
+                <th>Helfer/in</th>
+                <th>Erstelldatum</th>
+                <th>Points</th>
+            </tr>
+        </thead>
+        <tbody class="has-text-left">
+            <tr v-for="job in jobs" v-bind:key="job.value">
+                <td>{{ job.title }}</td>
+                <td>{{ job.matchedHelper ? job.matchedHelper.firstname : "Kein Helfer" }}</td>
+                <td>{{ job.created }}</td>
+                <td>0</td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script>
+export default {
+  name: "ClosedJobTable",
+  props: {
+      jobs: Array
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
